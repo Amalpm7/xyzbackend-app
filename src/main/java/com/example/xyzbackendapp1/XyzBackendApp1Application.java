494212926693729133ce139726545046cc394b01
Amalpm7 <amal.p@nestgroup.net>
2022-10-19 09:42:54 +0530

    first

---
+++ src/main/java/com/example/xyzbackendapp1/XyzBackendApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.xyzbackendapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzBackendApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzBackendApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xyzbackendapp1/XyzBackendApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xyzbackendapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzBackendApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
